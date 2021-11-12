7f911b38e16a02827e46fbb6c868b7b11c0f07d6
René Braun <rene.braun@fh-bielefeld.de>
2021-11-12 16:41:26 +0100

    OrthographicCamera implementiert

--- core/src/com/shooterman/game/Shooterman.java
+++ core/src/com/shooterman/game/Shooterman.java
@@ -1,31 +1,47 @@
 package com.shooterman.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class Shooterman extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
+	Texture map;
+	OrthographicCamera camera;
+	Sprite sprite;
 	
 	@Override
 	public void create () {
+		float w = Gdx.graphics.getWidth();
+		float h = Gdx.graphics.getHeight();
+		camera = new OrthographicCamera(1, h/w);
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		map = new Texture("Spielfeld.png");
+		sprite = new Sprite(TextureRegion.split(map, map.getWidth(), map.getHeight())[0][0]);
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
+		ScreenUtils.clear(0.1f, 0.35f, 0.1f, 1);
+		camera.position.x = sprite.getX() + sprite.getOriginX();
+		camera.position.y = sprite.getY() + sprite.getOriginY();
+		camera.zoom = 1200f; // Je größer der Zoom, desto weiterweg ist die Kamera
+		camera.update();
+		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(map, 0, 0);
 		batch.end();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+		map.dispose();
 	}
+
 }
