9fc4668941eea1ecf47b319b298c69dea3bd609e
mrdanf <dancoding@gmx.net>
2021-11-12 18:44:11 +0100

    Added Icon to GUI. Set height and width

--- desktop/src/com/shooterman/game/desktop/DesktopLauncher.java
+++ desktop/src/com/shooterman/game/desktop/DesktopLauncher.java
@@ -1,5 +1,6 @@
 package com.shooterman.game.desktop;
 
+import com.badlogic.gdx.Files;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.shooterman.game.Shooterman;
@@ -7,6 +8,12 @@ import com.shooterman.game.Shooterman;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Shooterman";
+		config.width = 950;
+		config.height = 950;
+		config.addIcon("128x128.png", Files.FileType.Internal);
+		config.addIcon("32x32.png", Files.FileType.Internal);
+		config.addIcon("16x16.png", Files.FileType.Internal);
 		new LwjglApplication(new Shooterman(), config);
 	}
 }
