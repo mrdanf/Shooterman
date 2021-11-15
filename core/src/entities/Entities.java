87abd9e06168d852588a83fc9b313c1398edff0e
Marcel <marcel_manfred_friedrich.sander@fh-bielefeld.de>
2021-11-15 16:09:43 +0100

    Erstellen der ersten entitäten
    Entitäten klasse hinzugefügt 
    entitäten package hinzugefügt unf player classe hinzugefügt

--- core/src/entities/Entities.java
+++ core/src/entities/Entities.java
@@ -1,2 +1,25 @@
-package entities;public class Entities {
+package entities;
+
+public class Entities {
+
+
+    public float x;
+
+
+
+    public float y;
+    public float getX() {
+        return x;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+    public float getY() {
+        return y;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
 }
--- core/src/entities/Player.java
+++ core/src/entities/Player.java
@@ -1,2 +1,11 @@
-package entities;public class Player {
+package entities;
+
+public class Player extends Entities{
+    int health;
+    int Movement;
+    int ammo;
+
+    public Player(int health){
+        this.health=health;
+    }
 }
