2e0fb824b73ba378ac0243f38c25c656bdde19f6
René Braun <rene.braun@fh-bielefeld.de>
2022-01-03 15:04:00 +0100

    Status Hud und Hilfefenster geupdated

--- core/src/hud/menu/buttons/HelpWindow.java
+++ core/src/hud/menu/buttons/HelpWindow.java
@@ -8,14 +8,17 @@ import hud.menu.AbstractWindow;
 
 public class HelpWindow extends AbstractWindow {
     private Label textLabel;
-    private static final String text = "Hilfe\n Spieler 1 WASD \t Spieler 2 IJKL";
+    private static final String text = "Hilfe\n " +
+            "                 Steuerung     Schießen\n"+
+            "Spieler 1 :   WASD             Strg \n" +
+            "Spieler 2 :    IJKL               LShift \n";
     public HelpWindow() {
         super(800, 800, new Color(0.6f, 0.6f, 0.6f, 1));
 
         Label.LabelStyle ls = new Label.LabelStyle();
         ls.font = new BitmapFont(Gdx.files.internal("msJhengHei.fnt"));
         textLabel = new Label(text, ls);
-        textLabel.setPosition(this.xOffset+30, this.yOffset+700);
+        textLabel.setPosition(this.xOffset+30, this.yOffset+500);
     }
 
     public Label getTextLabel() {
